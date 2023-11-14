7b9a7c4f171ea0b9f9abb7225bf40e571521c85a
Hogan McDonald <hogan.developer@gmail.com>
2023-11-14 14:14:24 -0600

    fixes up the header and footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,26 +1,27 @@
 import Container from './container'
-import { EXAMPLE_PATH } from '../lib/constants'
 
 const Footer = () => {
   return (
     <footer className="bg-neutral-50 border-t border-neutral-200">
       <Container>
-        <div className="py-28 flex flex-col lg:flex-row items-center">
-          <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Statically Generated with Next.js.
-          </h3>
-          <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
+        <div className="py-28 flex flex-col lg:flex-row items-center justify-between">
+          <a href="https://hogan.dev">
+            <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
+              hogan.dev
+            </h3>
+          </a>
+          <div className="flex flex-col lg:flex-row lg:justify-end justify-center items-center lg:pl-4 lg:w-1/2">
             <a
-              href="https://nextjs.org/docs/basic-features/pages"
+              href="https://www.linkedin.com/in/hogan-mcdonald"
               className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
-              Read Documentation
+              LinkedIn
             </a>
             <a
-              href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
+              href="https://github.com/HoganMcDonald"
               className="mx-3 font-bold hover:underline"
             >
-              View on GitHub
+              GitHub
             </a>
           </div>
         </div>
