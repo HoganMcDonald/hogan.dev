fead85a96ea5f4fe0adfc19f6a3734781908762a
Hogan McDonald <hogan.developer@gmail.com>
2023-11-14 14:16:21 -0600

    fixes index layout to match content preferences

--- components/hero-post.tsx
+++ components/hero-post.tsx
@@ -15,19 +15,14 @@ type Props = {
 
 const HeroPost = ({
   title,
-  coverImage,
   date,
   excerpt,
-  author,
   slug,
 }: Props) => {
   return (
     <section>
-      <div className="mb-8 md:mb-16">
-        <CoverImage title={title} src={coverImage} slug={slug} />
-      </div>
       <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
-        <div>
+        <div className="col-span-2">
           <h3 className="mb-4 text-4xl lg:text-5xl leading-tight">
             <Link
               as={`/posts/${slug}`}
@@ -40,10 +35,9 @@ const HeroPost = ({
           <div className="mb-4 md:mb-0 text-lg">
             <DateFormatter dateString={date} />
           </div>
-        </div>
-        <div>
-          <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
-          <Avatar name={author.name} picture={author.picture} />
+          <div>
+            {excerpt}
+          </div>
         </div>
       </div>
     </section>
--- components/post-preview.tsx
+++ components/post-preview.tsx
@@ -15,17 +15,11 @@ type Props = {
 
 const PostPreview = ({
   title,
-  coverImage,
   date,
-  excerpt,
-  author,
   slug,
 }: Props) => {
   return (
     <div>
-      <div className="mb-5">
-        <CoverImage slug={slug} title={title} src={coverImage} />
-      </div>
       <h3 className="text-3xl mb-3 leading-snug">
         <Link
           as={`/posts/${slug}`}
@@ -38,8 +32,6 @@ const PostPreview = ({
       <div className="text-lg mb-4">
         <DateFormatter dateString={date} />
       </div>
-      <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
-      <Avatar name={author.name} picture={author.picture} />
     </div>
   )
 }
