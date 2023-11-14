8ca2967c99d994524747b7f704670d13fd19929b
Hogan McDonald <hogan.developer@gmail.com>
2023-11-14 10:37:29 -0600

    customizes intro component

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,21 +1,17 @@
-import { CMS_NAME } from '../lib/constants'
+import Avatar from './avatar'
 
 const Intro = () => {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog.
+        Hogan.dev
       </h1>
-      <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{' '}
-        <a
-          href="https://nextjs.org/"
-          className="underline hover:text-blue-600 duration-200 transition-colors"
-        >
-          Next.js
-        </a>{' '}
-        and {CMS_NAME}.
-      </h4>
+      <div className="flex items-center mt-5">
+        <h4 className="text-center md:text-left text-lg mr-4 md:pl-8">
+          a blog by Hogan McDonald
+        </h4>
+        <Avatar picture="/assets/avatar.jpg" name="" />
+      </div>
     </section>
   )
 }
