bdb5cdf29febc7106ca487b785de5c922db195cd
Hogan McDonald <hogan.developer@gmail.com>
2023-11-15 12:44:59 -0600

    changes links to white

--- components/footer.tsx
+++ components/footer.tsx
@@ -13,13 +13,13 @@ const Footer = () => {
           <div className="flex flex-col lg:flex-row lg:justify-end justify-center items-center lg:pl-4 lg:w-1/2">
             <a
               href="https://www.linkedin.com/in/hogan-mcdonald"
-              className="mx-3 bg-black hover:bg-blue hover:text-black border border-black text-blue font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
+              className="mx-3 bg-black hover:bg-blue hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
               LinkedIn
             </a>
             <a
               href="https://github.com/HoganMcDonald"
-              className="mx-3 bg-black hover:bg-blue hover:text-black border border-black text-blue font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
+              className="mx-3 bg-black hover:bg-blue hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
               GitHub
             </a>
